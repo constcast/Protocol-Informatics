b822dcf122ecdbe6f2461b3f20f233d74649e8d9
Lothar Braun <braun@net.in.tum.de>
2011-11-21 16:19:52 +0100

    Utils module: show progress bar

---
+++ PI/util.py
@@ -0,0 +1,17 @@
+import sys
+
+# width defines bar width
+# percent defines current percentage
+def progress(width, percent):
+    marks = math.floor(width * (percent / 100.0))
+    spaces = math.floor(width - marks)
+ 
+    loader = '[' + ('=' * int(marks)) + (' ' * int(spaces)) + ']'
+ 
+    sys.stdout.write("%s %d%%\r" % (loader, percent))
+    if percent >= 100:
+        sys.stdout.write("\n")
+    sys.stdout.flush() 
+
+
+# vim: set sts=4 sw=4 cindent nowrap expandtab:
