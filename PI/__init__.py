a9fefc2bf39e26a242edb670657f1145f9cbee0c
Lothar Braun <braun@net.in.tum.de>
2011-11-23 20:58:12 +0100

    Import submodules from PI/__init__

--- PI/__init__.py
+++ PI/__init__.py
@@ -1,4 +1,9 @@
-__all__ = [ "input", "distance", "phylogeny", "multialign", "output", "core" ]
+import input
+import distance
+import phylogeny
+import multialign
+import output
+import core
 
 # vim: set sts=4 sw=4 cindent nowrap expandtab:
 
