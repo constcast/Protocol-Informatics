d8e6607f0753ea67f904871d8ad88ed941fc8a34
daubsi <daubsi@gmx.de>
2011-12-29 22:32:48 +0100

    Property inference step added

--- discoverer/core.py
+++ discoverer/core.py
@@ -1,71 +1,13 @@
-class Message:
-    # "Enums" for the token types
-    typeText = 0
-    typeBinary = 1
-    
-    _payload = None
-    tokenlist = []
-    
-    def __init__(self, payload, config):
-        self._payload = payload
-        self._config = config
-        self._analyze()
-        
-    def get_payload(self):
-        """Returns the payload of this message object"""
-        return self._payload
-    
-    def _analyze(self):
-        # Loop through the payload, memorize the index and create appropriate tupel
-        lastIsBinary = True
-        textSegmentLength = 0
-        textSegment = ""
-        curPos = 0
-        startsAt = 0
-        for char in self._payload:
-            curPos+=1
-            if self._config.ASCIILowerBound<=char and self._config.ASCIIUpperBound>=char:
-                if lastIsBinary:
-                    lastIsBinary = False;
-                    textSegmentLength = 0
-                    textSegment = ""
-                    startsAt = curPos
-                textSegmentLength+=1
-                textSegment+=chr(char)               
-            else:
-                if not lastIsBinary: # we finish a text segment
-                    if textSegmentLength<self._config.minWordLength:
-                        print "Word length to short"
-                    else:
-                        # We finish a word now
-                        tupel = (self.typeText, textSegment, startsAt, textSegmentLength)
-                        self.tokenlist.append(tupel)
-                        print tupel
-                        
-                else: # we read a binary
-                    
-                    tupel = (self.typeBinary, char, curPos, 1)
-                    self.tokenlist.append(tupel)
-                    print tupel
-
-                lastIsBinary = True
-        # Finish unfinished text segments
-        if not lastIsBinary:
-            if textSegmentLength<self._config.minWordLength:
-                print "Word length to short"
-            else:
-                # We finish a word now
-                tupel = (self.typeText, textSegment, startsAt, textSegmentLength)
-                self.tokenlist.append(tupel)
-        print self.tokenlist
-            
 import curses
 
 class Message:
     # "Enums" for the token types
     typeText = 0
     typeBinary = 1
-                
+       
+    def __repr__ (self): 
+        return 'Msg: %s' % (self.tokenlist)
+     
     def __init__(self, payload, config):
         self._payload = payload
         self._config = config
@@ -75,9 +17,25 @@ class Message:
     def get_payload(self):
         """Returns the payload of this message object"""
         return self._payload
+    
     def get_tokenlist(self):
         return self.tokenlist
     
+    def get_tokenrepresentation(self):
+        l = []
+        for type,raw,start,end in self.tokenlist:
+            l.append(type)
+        t = tuple(l)
+        return t
+    
+    def get_tokenrepresentation_string(self):
+        l = []
+        map = {0:"text", 1:"binary"}
+        for type,raw,start,end in self.tokenlist:
+            l.append(map[type])
+        t = tuple(l)
+        return t
+      
     def _analyze(self):
         # Loop through the payload, memorize the index and create appropriate tupel
         lastIsBinary = True
--- discoverer/setup.py
+++ discoverer/setup.py
@@ -1,5 +1,7 @@
 import discoverer
 
+cluster = {}
+
 def setup(flowBasedSequences, config):
     if flowBasedSequences==None:
         print "FATAL: No sequences loaded yet"
@@ -10,6 +12,50 @@ def setup(flowBasedSequences, config):
             for seq in flowInfo.sequences:
                 newMessage = discoverer.core.Message(seq.sequence, config)
                 #print newMessage.get_payload()
-                print "Tokenlist of ", seq.sequence, " = ", newMessage.get_tokenlist()
+                #print "Tokenlist of ", seq.sequence, " = ", newMessage.get_tokenrepresentation_string()
+                # Cluster message
                 
-                
+                newrep = newMessage.get_tokenrepresentation_string()
+                if not cluster.has_key(newrep):
+                    cluster.update({newrep: [newMessage]})
+                else:
+                    l = cluster.get(newrep)
+                    l.append(newMessage)
+                    cluster.update({newrep: l})
+            
+    # Print cluster
+    keys = cluster.keys()
+    for key in keys:
+        l = cluster.get(key)
+        print "Key:", key, " Elements: ", l  
+        
+    # Step 1 finished
+    
+    # Perform format inference
+    # Walk through every cluster and check for variable/constant properties
+    print '+++++++++++++++++++++++++++++++++++'
+    keys = cluster.keys()
+    for key in keys:
+        l = cluster.get(key)
+        print "Key:", key, " Elements: ", l  
+        # get prototypes from first list elemnt
+        prototypeMessage = l[0]
+        tokenList = prototypeMessage.get_tokenlist()
+        tokenIndex = 0
+        result = []
+        for type, value, begin, end in tokenList:
+            # Now we have the value, lets check every other message for the same value
+            constant = True
+            for message in l:
+                tl = message.get_tokenlist()
+                t,v,b,e = tl[tokenIndex]
+                if not value==v:
+                    constant = False
+                    break
+            if constant:
+                result.append("const")
+            else:
+                result.append("variable")
+            tokenIndex +=1
+        print "Result for cluster after property inference: ", result
+                      
