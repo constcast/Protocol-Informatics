a06c7520e92a0d878ae2bb43a1ec26e0397afe47
Lothar Braun <braun@net.in.tum.de>
2012-09-07 14:46:15 +0200

    Removed daubsi-specific files
    They live on in branch daubsi-content

--- convert_bro_to_testdata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# First concat client and server parts by: 'cat client.bro server.bro > allinone.bro
-# Then call this script with ./convert_bro_to_testdata allinone.bro
-
-cat $1 | awk {'print $2 " " $3 " " $4 " " $6'} | sort -k 1,2 | awk {'print $1 " " $4'} > $1_test
--- printErrors.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-NUM=9; BASE=dns_2000; for X in `seq 0 $[NUM-1]`; do { cd dns_${X}; ANZ=`ls | wc -l`; for IDX in `seq 0 $[ANZ-1]`; do { head -n 3 ${BASE}_${IDX}_testresults.txt | tail -n 1 | awk '{printf "%s & ",$8;} '; } done; echo; cd ..; } done
-
-
-
-NUM=91; BASE=dns_1000; for X in `seq 0 $[NUM-1]`; do { cd ${BASE}_${X}; ANZ=`ls | wc -l`; echo -n dns\\_${X}\ \&\ ; for IDX in `seq 0 $[ANZ-1]`; do { head -n 3 ${BASE}_${IDX}_testresults.txt | tail -n 1 | awk '{printf "%s & ",$8;} '; } done; echo; cd ..; } done
